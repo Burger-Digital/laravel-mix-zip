fe50c15c3f6f573e3aa53018316dc6d22c68a5d0
Nathan Heinrich <nathan@fweb.com.au>
2021-01-08 16:43:36 +1000

    wip

--- index.js
+++ index.js
@@ -26,33 +26,35 @@ class ZipPlugin {
 	}
 
 	register(dirs, files, output_path) {
-		const output = file_system.createWriteStream(output_path);
+		mix.after(() => {
+			const output = file_system.createWriteStream(output_path);
 
-		const archive = archiver('zip');
+			const archive = archiver('zip');
 
-		output.on('close', function () {
-			console.log(archive.pointer() + ' total bytes');
-			console.log('Theme build output to /build/deploy/theme.zip.');
+			output.on('close', function () {
+				console.log(archive.pointer() + ' total bytes');
+				console.log('Theme build output to /build/deploy/theme.zip.');
+			});
+
+			archive.on('error', function(err){
+				throw err;
+			});
+
+			archive.pipe(output);
+
+			// append files from a sub-directory, putting its contents at the root of archive
+			dirs.forEach((dir) => {
+				const absolute = path.join(__dirname,'/../' + dir);
+				archive.directory(absolute, dir);
+			});
+
+			files.forEach((file) => {
+				const absolute = path.join(__dirname,'/../' + file);
+				archive.file(absolute, {name: file});
+			});
+
+			archive.finalize();
 		});
-
-		archive.on('error', function(err){
-			throw err;
-		});
-
-		archive.pipe(output);
-
-		// append files from a sub-directory, putting its contents at the root of archive
-		dirs.forEach((dir) => {
-			const absolute = path.join(__dirname,'/../' + dir);
-			archive.directory(absolute, dir);
-		});
-
-		files.forEach((file) => {
-			const absolute = path.join(__dirname,'/../' + file);
-			archive.file(absolute, {name: file});
-		});
-
-		archive.finalize();
 	}
 }
 
