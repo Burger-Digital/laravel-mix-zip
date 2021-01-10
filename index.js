392426e520e65b5b2dbc17b0ff8276dde193b34a
Nathan Heinrich <nathan_heinrich@outlook.com>
2021-01-10 13:54:36 +1000

    Release 1.0.2

--- index.js
+++ index.js
@@ -4,59 +4,72 @@ const path = require('path');
 const mix = require('laravel-mix');
 
 class ZipPlugin {
-	/**
-	 * The optional name to be used when called by Mix.
-	 * Defaults to the class name, lowercased.
-	 *
-	 * Ex: mix.example();
-	 *
-	 * @return {String|Array}
-	 */
-	name() {
-		return 'zip';
-	}
+    /**
+     * The optional name to be used when called by Mix.
+     * Defaults to the class name, lowercased.
+     *
+     * Ex: mix.example();
+     *
+     * @return {String|Array}
+     */
+    name() {
+        return 'zip';
+    }
 
-	/**
-	 * All npm dependencies that should be installed by Mix.
-	 *
-	 * @return {Array}
-	 */
-	dependencies() {
-		return ['archiver'];
-	}
+    /**
+     * All npm dependencies that should be installed by Mix.
+     *
+     * @return {Array}
+     */
+    dependencies() {
+        return ['archiver'];
+    }
 
-	register(dirs, files, output_path) {
-		mix.after(() => {
-			const output = file_system.createWriteStream(output_path);
+    /**
+     * Register the component.
+     *
+     * When your component is called, all user parameters
+     * will be passed to this method.
+     *
+     * @param  {Array} directories
+     * @param  {Array} files
+     * @param  {string} output_path
+     * @return {void}
+     *
+     */
+    register(directories, files, output_path) {
+        mix.after(() => {
+            const output = file_system.createWriteStream(
+                mix.paths.root(output_path)
+            );
 
-			const archive = archiver('zip');
+            const archive = archiver('zip');
 
-			output.on('close', function () {
-				console.log(archive.pointer() + ' total bytes');
-				console.log('Theme build output to /build/deploy/theme.zip.');
-			});
+            output.on('close', function () {
+                console.log(`Wrote ${output_path} -  ${archive.pointer()} bytes`);
+            });
 
-			archive.on('error', function(err){
-				throw err;
-			});
+            archive.on('error', function (err) {
+                throw err;
+            });
 
-			archive.pipe(output);
+            archive.pipe(output);
 
-			// append files from a sub-directory, putting its contents at the root of archive
-			dirs.forEach((dir) => {
-				const absolute = path.join(__dirname,'/../' + dir);
-				archive.directory(absolute, dir);
-			});
+            // append files from a sub-directory, putting its contents at the root of archive
+            directories.forEach((directory) => {
+                const absolute = mix.paths.root(directory);
+                archive.directory(absolute, directory);
+            });
 
-			files.forEach((file) => {
-				const absolute = path.join(__dirname,'/../' + file);
-				archive.file(absolute, {name: file});
-			});
+            files.forEach((file) => {
+                const absolute = mix.paths.root(file);
+                archive.file(absolute, {name: file});
+            });
 
-			archive.finalize();
-		});
-	}
+            archive.finalize();
+        });
+    }
 }
 
-mix.extend('split', new ZipPlugin());
+mix.extend('zip', new ZipPlugin());
 
